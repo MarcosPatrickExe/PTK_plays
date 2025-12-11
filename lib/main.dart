1610d324e392964233438aca076f99655fa28164
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2025-12-10 22:54:49 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'PTK plays app',
+      title: 'PTK plays',
       theme: ThemeData(
        
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
