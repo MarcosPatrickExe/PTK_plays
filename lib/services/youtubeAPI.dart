f5ec4a2c20986ea961d220c052747160f5a0e735
Marcos Patrick <marcospatrick039474@gmail.com>
2025-12-13 14:59:53 -0300

    adicionando lib 'http' no projeto

--- lib/services/youtubeAPI.dart
+++ lib/services/youtubeAPI.dart
@@ -1,9 +1,25 @@
-import 'package:flutter/foundation.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
-class YouTubeAPI {
+final String apiKey = "YOUR_API_KEY";
+final String channelId = "YOUR_CHANNEL_ID"; // e.g., "UC_Fh8jueZhKYjcSNgbQMzog" for Flutter
+
+Future<List<String>> fetchChannelVideoIds() async {
+  // 1. First, find the 'uploads' playlist ID for the channel
+  String channelUrl = 'https://www.youtube.com/@patrickson_plays';
+  var channelResponse = await http.get(Uri.parse(channelUrl));
+  var channelData = json.decode(channelResponse.body);
+  String uploadsPlaylistId = channelData['items'][0]['contentDetails']['relatedPlaylists']['uploads'];
+
+  // 2. Then, fetch videos from that playlist
+  String playlistUrl = 'www.googleapis.com';
+  var playlistResponse = await http.get(Uri.parse(playlistUrl));
+  var playlistData = json.decode(playlistResponse.body);
+
+  List<String> videoIds = [];
   
-  Future<bool> getVideos() {
-    
-    return Future.value(false);
+  for (var item in playlistData['items']) {
+    videoIds.add(item['snippet']['resourceId']['videoId']);
   }
+  return videoIds;
 }
