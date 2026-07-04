fa655a4d5bcd87ce823bdd9807555a17713f89ed
Marcos Patrick <marcospatrick039474@gmail.com>
2026-07-04 14:44:59 -0300

    lendo a api key do .env de acordo com a plataforma (android/ios/web)

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,6 +1,20 @@
+import 'package:flutter/foundation.dart' show kIsWeb;
+import 'dart:io' show Platform;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 class Utils {
- 
-  static const String APIkey = "AIzaSyBpKkubdXt7tlbwOPR5TkVYQ22_X0byau4";
+
+  static String get APIkey {
+    if (kIsWeb) {
+      return dotenv.env['YT_API_KEY_WEB'] ?? '';
+    } else if (Platform.isAndroid) {
+      return dotenv.env['YT_API_KEY_ANDROID'] ?? '';
+    } else if (Platform.isIOS) {
+      return dotenv.env['YT_API_KEY_IOS'] ?? '';
+    }
+    return '';
+  }
+
   static const String channelID = "UCB0Xu_75SQQIHVjaTmGBYuQ";
-  
+
 }
