a377a7032bd5d14fba3f01ead8589481dd1ac293
Marcos Patrick <marcospatrick039474@gmail.com>
2026-07-05 02:38:24 -0300

    injetando o AuthViewModel no fluxo principal do app

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:ptk_plays/data/repositories/AuthRepository.dart';
 import 'package:ptk_plays/data/repositories/YouTubeRepository.dart';
 import 'package:ptk_plays/data/services/YouTubeService.dart';
 import 'package:ptk_plays/firebase_options.dart';
 import 'package:ptk_plays/utils/ThemeController.dart';
 import 'package:ptk_plays/utils/app_theme.dart';
+import 'package:ptk_plays/viewmodels/AuthViewModel.dart';
 import 'package:ptk_plays/viewmodels/YoutubeVideoModel.dart';
 import 'view/SplashScreen.dart';
 import './utils/utils.dart';
@@ -36,14 +38,17 @@ class MyApp extends StatelessWidget {
     YouTubeService ytServi = YouTubeService( Utils.APIkey );
     YouTubeRepository ytRepo = YouTubeRepository( ytServi );
     YoutubeViewModel ytVM = YoutubeViewModel(ytRepo);
-    
+
+    AuthRepository authRepo = AuthRepository();
+    AuthViewModel authVM = AuthViewModel(authRepo);
+
     return MaterialApp(
       title: 'PTK plays',
       debugShowCheckedModeBanner: true,
       theme: AppThemes.lightTheme,
       darkTheme: AppThemes.darkTheme,
       themeMode: context.read<ThemeController>().getThemeMode,
-      home: SplashScreen( viewmodelYTtemp: ytVM, apiKEYtemp: Utils.APIkey ),
+      home: SplashScreen( viewmodelYTtemp: ytVM, apiKEYtemp: Utils.APIkey, authViewModelTemp: authVM ),
     );
     
   }
